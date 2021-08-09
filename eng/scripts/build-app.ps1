bba9e8f7c14e36307c8c5725091bcb308b29c42d
Reuben <reuben.hillyer@hotmail.co.uk>
2021-08-09 21:27:38 +0100

    Exclude the WinUI project from the solution.

--- eng/scripts/build-app.ps1
+++ eng/scripts/build-app.ps1
@@ -1,1 +1,4 @@
+# Expect working directory to be ./src
 dotnet build
+
+Write-Host "##vso[task.logissue type=warning]The WinUI project is not being built. Not currently supported. Intentionally excluded from the solution file."
